7e2fe3eb76833c6a1c65ca38c5b4b9df73496c96
Nathan Broadbent <nathan.f77@gmail.com>
2010-06-20 01:51:27 +1200

    Added ajax, and jquery ui libraries.

---
+++ public/javascripts/main.js
@@ -0,0 +1,80 @@
+function setRefresh() {
+    window.setTimeout("ajaxRefresh(true);",7000);
+    window.setTimeout("fakeTimerCountdown();",1000);
+}
+
+function ajaxRefresh(repeat) {
+    $("#spinner").show();
+    $.getJSON('info.json', function(data) {
+        $("#spinner").hide();
+        $('#station-link').html(data.station_link);
+        $('#artist-link').html(data.artist_link);
+        $('#title-link').html(data.title_link);
+        $('#album-link').html(data.album_link);
+        $('#album-image').html(data.album_image);
+        $("#volumeslider").slider("value", parseInt(data.volume));
+        remain_s = data.remain_s;
+        total_s = data.total_s;
+        updateRemainingtime(remain_s, total_s);
+        // Reset the timer to repeat.
+        if (repeat==true) {
+            window.setTimeout("ajaxRefresh(true);",7000);
+        }
+        // update browser title.
+        $('title').html(data.artist + " - " + data.title + " - shell.fm");
+    });
+}
+
+function ajaxCommand(cmd) {
+    // set icon to spinner
+    $("#"+cmd).attr("src", "/images/ui-anim_basic_16x16.gif");
+    $.get('/cmd/'+cmd,{},function(response){
+        $('#'+cmd).attr('src', '/images/'+cmd+'.png'); // set icon back to normal
+        setTimeout("ajaxRefresh(false);", 2000); // refresh after delay
+        // fade in the flash message, for 5 seconds
+        $('.flash').html(response);
+        $('.flash').slideDown();
+        setTimeout("$('.flash').slideUp();", 5000);
+    })
+}
+
+function ajaxPlay(station, prefix) {
+    $("#playspinner").show();
+    var url = prefix+station;
+    $.get('/cmd/play?station='+url,{},function(response){
+        $("#playspinner").hide();
+        setTimeout("ajaxRefresh(false);", 2000); // refresh after delay
+
+        // fade in the flash message, for 5 seconds
+        $('.flash').html(response);
+        $('.flash').slideDown();
+        setTimeout("$('.flash').slideUp();", 5000);
+    })
+
+}
+
+function pad2(number) {
+    return (number < 10 ? '0' : '') + number;
+}
+
+function updateRemainingtime(s, t) {
+    $("#progresstime").progressbar("value", (t - s) / t * 100);
+    var sMins = parseInt(s / 60);
+    var sSecs = parseInt(s % 60);
+    var tMins = parseInt(t / 60);
+    var tSecs = parseInt(t % 60);
+    document.getElementById("remainlabel").innerHTML = pad2(sMins) + ":" + pad2(sSecs);
+    document.getElementById("totallabel").innerHTML = pad2(tMins) + ":" + pad2(tSecs);
+}
+
+function fakeTimerCountdown() {
+    if (remain_s > 0) {
+        remain_s -= 1;
+        updateRemainingtime(remain_s, total_s);
+    }
+    // we must keep the repeating timer rolling, even if time == 0
+    window.setTimeout("fakeTimerCountdown()",1000);
+}
+
+function volumeChange() { return false;}
+
--- shell-fm_sinatra.rb
+++ shell-fm_sinatra.rb
@@ -45,31 +45,6 @@ get '/' do
   @client_ip = @env['REMOTE_ADDR']    # Displays clients IP
   @flash = nil  # initialize a '@flash' var to display flash messages on the generated html
 
-  # Do something depending on the command given (if any).
-  case params[:cmd]
-  when "pause", "skip", "love", "ban", "stop"
-    shellfmcmd(params[:cmd])
-    @flash = "Sent shell.fm the '#{CmdTitles[params[:cmd]]}' command."
-  when "start"
-    `sudo pkill shell-fm 2>/dev/null`
-    `sudo shell-fm`
-    @flash = "(Re)started shell.fm process."
-  when "kill"
-    `sudo pkill shell-fm 2>/dev/null`
-    @flash = "Stopped shell.fm process."
-  when "play"
-    # convert any "" from unused fields to nil. (because "" ~~ true in ruby.)
-    [:stationselect, :bookmarkselect, :stationurl].each do |k|
-      params[k] = nil if params[k] == ""
-    end
-    # Set the station var to the first param with a non-null value
-    station = params[:stationselect] ||
-              params[:bookmarkselect] ||
-              params[:stationurl]
-    shellfmcmd("play lastfm://#{station}")
-    @flash = "Changed shell.fm station to: '#{station}'"
-  end
-
   # Gets track info from shell.fm network interface. If successful, continues
   if i = get_info
     i[:image_url] = nil if i[:image_url] == ""
@@ -83,8 +58,60 @@ get '/' do
 
   puts @title_link
   @track_info = i
+
+  return params[:format] if params[:format]
   erb :index
 end
+
+
+get '/info.json' do
+  if i = get_info
+    i[:image_url] = nil if i[:image_url] == ""
+    @station_link = link_to(i[:station_url], i[:station])
+    @artist_link  = link_to(i[:artist_url],  i[:artist])
+    @title_link   = link_to(i[:title_url],   i[:title])
+    @album_link   = link_to(i[:album_url],   i[:album])
+    @album_image  = i[:image_url] ? "<img src='#{i[:image_url]}'>" :
+                    "<div id=\"noimage\">No Album Image.</div>"
+  return %Q{
+    {
+    "artist": "#{i[:artist]}",
+    "title": "#{i[:title]}",
+    "station_link": "#{@station_link}",
+    "artist_link": "#{@artist_link}",
+    "title_link": "#{@title_link}",
+    "album_link": "#{@album_link}",
+    "album_image": "#{@album_image}",
+    "remain_s": #{i[:remain_s]},
+    "total_s": #{i[:total_s]},
+    "volume": #{i[:volume].to_i}
+    }
+  }
+  else
+    return false
+  end
+end
+
+get '/cmd/:cmd' do
+  # Do something depending on the command given (if any).
+  case params[:cmd]
+  when "pause", "skip", "love", "ban", "stop"
+    shellfmcmd(params[:cmd])
+    @flash = "Sent shell.fm the '#{CmdTitles[params[:cmd]]}' command."
+  when "start"
+    `sudo pkill shell-fm 2>/dev/null`
+    `sudo shell-fm`
+    @flash = "(Re)started shell.fm process."
+  when "kill"
+    `sudo pkill shell-fm 2>/dev/null`
+    @flash = "Stopped shell.fm process."
+  when "play"
+    shellfmcmd("play lastfm://#{params[:station]}")
+    @flash = "Changed shell.fm station to: '#{params[:station]}'"
+  end
+  return @flash
+end
+
 
 # Shows a page containing shell-fm.rc. (with password removed, and "<BR>" instead of "\n")
 get '/config' do
@@ -109,13 +136,13 @@ def get_info
   info = []
   # Try to get track info 2 times. (just in case of a random TCP error on the first attempt)
   2.times do
-    info = shellfmcmd("info %S||%s||%A||%a||%T||%t||%L||%l||%I||%r||%f||%v")
+    info = shellfmcmd("info %S||%s||%A||%a||%T||%t||%L||%l||%I||%r||%f||%v||%R||%d")
     break if info
   end
   return false unless info
   info = info.split("||")
   k = %w(station_url station artist_url artist title_url title
-         album_url album image_url remaining duration volume)
+         album_url album image_url remaining duration volume remain_s total_s)
   info_hash = {}
   info.each_with_index {|v, i| info_hash[k[i].to_sym] = v}
   return info_hash
